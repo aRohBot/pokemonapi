cd4cef646f39908eebd04a1a78e0a956407bae35
Alex Roh <imajndao@gmail.com>
2022-09-26 14:23:29 -0700

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [pokemon,setPokemon] = useState([])
+
+  useEffect(()=>{
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+    .then((result)=>{
+      return result.json()
+    }).then((res)=>{
+      console.log(res)
+      setPokemon(res.results)
+    }).catch((error)=>{
+      console.log(error)
+    })
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          pokemon.map((pokemon)=>(
+            <div>
+              <li>{pokemon.name}</li>
+            </div>
+          ))
+        }
+
+
+
     </div>
   );
 }
